228a24c8f4d16a14bcf3b308e71a552b36b7a906
Jeremy Butler <jeremy_butler@shi.com>
2024-10-30 23:55:36 -0700

    working to make this work....

--- PowerShell/dnsAssessmentScript.ps1
+++ PowerShell/dnsAssessmentScript.ps1
@@ -6,7 +6,6 @@
     This script is designed to query all available subscriptions within an Azure tenant for any network related resources.
     The script will output a .csv file with the following columns: ResourceGroupName, Name, Type, Location.
 
-
 .NOTES
     Author: Jeremy Butler
     Date: 2021-10-30
@@ -24,12 +23,23 @@ foreach ($subscription in $subscriptions) {
     # Set the current subscription context
     Set-AzContext -SubscriptionId $subscription.Id
 
-    # Get all network-related resources
+    # Get network-related resources
     $vnetworks = Get-AzVirtualNetwork
     $nsgs = Get-AzNetworkSecurityGroup
     $publicIps = Get-AzPublicIpAddress
     $loadBalancers = Get-AzLoadBalancer
     $appGateways = Get-AzApplicationGateway
+    $firewall = Get-AzFirewall
+    $bastionHosts = Get-AzBastionHost
+    $dnsZones = Get-AzDnsZone
+    $expressRouteCircuits = Get-AzExpressRouteCircuit
+    $firewallPolicies = Get-AzFirewallPolicy
+    $networkGateways = Get-AzVirtualNetworkGateway
+    $natGateways = Get-AzNatGateway
+    $privateDnsZones = Get-AzPrivateDnsZone
+    $privateEndpoints = Get-AzPrivateEndpoint
+    $routeTables = Get-AzRouteTable
+    $vpnGateways = Get-AzVpnGateway
     
 
     # Add the resources to the array
@@ -39,7 +49,42 @@ foreach ($subscription in $subscriptions) {
     $networkResources += $loadBalancers
     $networkResources += $appGateways
     $networkResources += $subscription
+    $networkResources += $firewall
+    $networkResources += $bastionHosts
+    $networkResources += $dnsZones
+    $networkResources += $expressRouteCircuits
+    $networkResources += $firewallPolicies
+    $networkResources += $networkGateways
+    $networkResources += $natGateways
+    $networkResources += $privateDnsZones
+    $networkResources += $privateEndpoints
+    $networkResources += $routeTables
+    $networkResources += $vpnGateways
 }
 
 # Output all network resources
-$networkResources | Format-Csv -Property Subscription, ResourceGroupName, Name, Type, Location | Out-File -FilePath "./networkResources.csv"
+
+# $networkResources += [PSCustomObject]@{
+#     Name             = $networkResources.Name
+#     ResourceGroup    = $networkResources.ResourceGroupName
+#     Location         = $networkResources.Location
+#     ResourceType     = $networkResources.ResourceType
+#     SubscriptionName = $subscription.name
+#     Subscription     = $subscription.Id
+# }
+
+$exportFilePath = Join-Path -Path (Get-Location) -ChildPath "CspResources.csv"
+$networkResources | Export-Csv -Path $exportFilePath -NoTypeInformation
+
+Write-Output "Analysis complete. Please check the output above for resource readiness details."
+Write-Output "Exported resource list to $exportFilePath"
+
+
+
+
+
+
+
+
+#$networkResources | Export-Csv -Path "./networkResources.csv"
+
---
+++ PowerShell/inventory.ps1
@@ -0,0 +1,49 @@
+<#
+.SYNOPSIS
+    This PowerShell script queries all Azure subscriptions available to the logged in user and produces a .csv file output with all Azure resources.
+
+.DESCRIPTION
+    This script is designed to query all available subscriptions within an Azure tenant for resources currently.
+    The script will output a .csv file with the following columns: Name, Resource Group, Location, Resource Type, SubscriptionName, and Subscription.
+
+.NOTES
+    Author: Jeremy Butler
+    Date: 2021-09-27
+    Version: 0.1
+#>
+
+# Get a list of subscriptions and prompt user to select one
+$subscriptions = Get-AzSubscription
+
+# Initialize an array to hold the resource details with readiness status
+$resourceDetails = @()
+
+foreach ($subscription in $subscriptions) {
+
+set-azcontext $subscription.id
+
+# Get the list of resources in the subscription
+$resources = Get-AzResource
+
+
+# Perform analysis for each resource
+foreach ($resource in $resources) {
+    $resourceType = $resource.ResourceType
+    $resourceId = $resource.ResourceId
+    $resourceDetails += [PSCustomObject]@{
+        Name             = $resource.Name
+        ResourceGroup    = $resource.ResourceGroupName
+        Location         = $resource.Location
+        ResourceType     = $resource.ResourceType
+        SubscriptionName = $subscription.name
+        Subscription     = $subscription.Id
+    }
+}
+
+
+# Export resource details to a CSV file in the current directory
+$exportFilePath = Join-Path -Path (Get-Location) -ChildPath "CspResources.csv"
+$resourceDetails | Export-Csv -Path $exportFilePath -NoTypeInformation
+Write-Output "Analysis complete. Please check the output above for resource readiness details."
+Write-Output "Exported resource list to $exportFilePath"
+}
